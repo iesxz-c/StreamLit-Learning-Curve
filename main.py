05e83c993d28c5339f933cbd8a96d467e02ea7ff
Akash <akash.krishnan.046@gmail.com>
2024-11-29 17:32:39 +0530

    Callback

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ if "step" not in st.session_state:
     st.session_state.step =1
 if "info" not in st.session_state:
     st.session_state.info ={}
-    
+
+def go_to_step2(name):
+    st.session_state.info["name"] = name
+    st.session_state.step = 2
+
 if st.session_state.step ==1:
     st.header("Part 1 : Info")
     name = st.text_input("Name", value=st.session_state.info.get("name",""))
-    if st.button("Next") :
-        st.session_state.info["name"] = name
-        st.session_state.step = 2
+    st.button("Next",on_click=go_to_step2,args=(name,))
         
+### Callback run before any other code on the next rerun
+
+
+
+
 if st.session_state.step ==2:
     st.header("Part 2 : Review")
     st.subheader("PLease review this")
