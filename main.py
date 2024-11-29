cfff2b9d053b4bbe19e8f2373b8f9f789a4a480e
Akash <akash.krishnan.046@gmail.com>
2024-11-29 18:29:03 +0530

    Cache

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-## Layouts
+
 import streamlit as st
+import time
+#caching
 
-st.sidebar.title("THis a sidebar")
-st.sidebar.write("Fui")
-side = st.sidebar.text_input("Hi")
-
-a,b,c  = st.tabs(["1","2","3"])
-with a:
-    st.write("1")
-with b:
-    st.write("2")
-with c:
-    st.write("3")
-
-e,f = st.columns(2)
-
-with e:
-    st.write("HJINXcbmcjhcbckhcbj")
-with f:
-    st.write("bvhgcjbvjhcm bjvb cmsgumc hcbysc kc hcmd cijhckn cnd cmhcicckshjcc")
-
-with st.container(border=True):
-    st.write("Hello, world!")
-
-place = st.empty()
-place.write("bjhbvmdjbvjhbvhbm")
-
-if st.button("Override"):
-    place.write("hjcbhjcbhjcbhjcbhjcbh")
-
-with st.expander("Expand"):
-    st.write("This is an expander")
-
-st.button("Button", help="Button")
+@st.cache_data(ttl=60)
+    # cache the data for 60s
+def fetchd():
+    time.sleep(3)
+    return {"data":"Cached data"}
+st.write("Fetching data..................")
+data = fetchd()
+st.write(data) 
